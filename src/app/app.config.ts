bd6d8c5ee0c81109bdb8c62f3a58f231ea4fd281
Lucas Wang <lucaschiwang@gmail.com>
2024-03-28 15:08:05 +1100

    Added firebase and updated auth component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'books-5da30',
+          appId: '1:201734349635:web:7e1a5f57e2a26f872f05f6',
+          storageBucket: 'books-5da30.appspot.com',
+          apiKey: 'AIzaSyC4CtWOi6qKe85HpR19xiC_5YL_iQBh23g',
+          authDomain: 'books-5da30.firebaseapp.com',
+          messagingSenderId: '201734349635',
+          measurementId: 'G-NGCM4R6PCC',
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+  ],
 };
--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: new FormControl('test', [Validators.required]),
+  });
 
+  isAuthenticated = false;
+
+  constructor(private fb: FormBuilder, protected auth: Auth) {}
+  ngOnInit(): void {
+    this.auth.onAuthStateChanged((user) => {
+      console.log(user);
+      console.log('current user:', this.auth.currentUser);
+    });
+  }
+
+  onSubmit() {
+    signInWithEmailAndPassword(
+      this.auth,
+      this.form.value.email!,
+      this.form.value.password!
+    );
+  }
 }
